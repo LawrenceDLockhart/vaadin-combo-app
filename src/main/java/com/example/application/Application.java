c8c63fcbe51c0a7e89936b5d2e324886dfea196a
Lawrence Lockhart <lawrence@vaadin.com>
2024-03-21 16:19:56 -0500

    Add server push example

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "my-app")
+@Push
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
