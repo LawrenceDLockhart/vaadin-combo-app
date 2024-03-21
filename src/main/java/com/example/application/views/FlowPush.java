50b53fe7848264e3c9e1f8b0b153f7c5fb19ea55
Lawrence Lockhart <lawrence@vaadin.com>
2024-03-21 16:19:47 -0500

    Add file

---
+++ src/main/java/com/example/application/views/FlowPush.java
@@ -0,0 +1,18 @@
+package com.example.application.views;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import reactor.core.publisher.Flux;
+import java.time.Duration;
+
+@Route("flow-push")
+public class FlowPush extends VerticalLayout {
+    public FlowPush() {
+        var ui = UI.getCurrent();
+
+        Flux.fromArray("Full stack from the back!".split(" "))
+                .delayElements(Duration.ofSeconds(1))
+                .subscribe(word -> ui.access(() -> add(word+ " ")));
+    }
+}
