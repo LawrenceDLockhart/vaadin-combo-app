284b35ad849cf76953d37bc82f863299236c805b
Lawrence Lockhart <lawrence@vaadin.com>
2024-03-26 14:01:50 -0500

    Add dark theme, add form to Flow form view

--- src/main/java/com/example/application/views/FlowForm.java
+++ src/main/java/com/example/application/views/FlowForm.java
@@ -1,7 +1,9 @@
 package com.example.application.views;
 
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -11,7 +13,23 @@ import javax.swing.text.FlowView;
 public class FlowForm extends VerticalLayout {
 
     public FlowForm() {
-        add(new TextField("Enter name"));
+        TextField firstName = new TextField("First name");
+        TextField lastName = new TextField("Last name");
+        TextField username = new TextField("Username");
+        PasswordField password = new PasswordField("Password");
+        PasswordField confirmPassword = new PasswordField("Confirm password");
 
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(firstName, lastName, username, password,
+                confirmPassword);
+        formLayout.setResponsiveSteps(
+                // Use one column by default
+                new FormLayout.ResponsiveStep("0", 1),
+                // Use two columns, if layout's width exceeds 500px
+                new FormLayout.ResponsiveStep("500px", 2));
+// Stretch the username field over 2 columns
+        formLayout.setColspan(username, 2);
+
+        add(formLayout);
     }
 }
